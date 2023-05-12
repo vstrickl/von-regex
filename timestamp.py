92d5265e4f97109209ac2f925ce633cceb575f83
Vonique Stricklen <vstrickl.git@gmail.com>
2023-05-11 21:42:38 -0700

    Adjusted code.

--- timestamp.py
+++ timestamp.py
@@ -1,7 +1,7 @@
 import re
 
-with open('transcripts/Mom_2023_Clip-1.txt') as f:
-    fin = f.read()
+f = open('transcripts/Mom_2023_Clip-1.txt')
+fin = f.read()
   
 z = re.sub(pattern="\d{2};\d{2};\d{2};\d{2}\s-\s\d{2};\d{2};\d{2};\d{2}",repl="", string=fin)
 print(z)
